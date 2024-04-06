c98b7f52d647ba9e345b27f195995d3c46d49691
aadic0 <aadichauhan321@gmail.com>
2024-04-06 00:51:07 -0600

    Third API endpoint implemented

--- my-ecommerce-app/src/component/ProductList.js
+++ my-ecommerce-app/src/component/ProductList.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from './ProductItem';
-import productsData from '../data/products';
 
 export default function ProductList({ addToCart }) {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(response => response.json())
+      .then(data => setProductsData(data))
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div>
       {productsData.map(product => (
